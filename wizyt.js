8fd4db5277a5cda4942ae814afc0a63e8c62402e
eryk-lawniczak <35136216+eryk-lawniczak@users.noreply.github.com>
2018-09-21 11:25:10 +0200

    Add files via upload

---
+++ wizyt.js
@@ -0,0 +1,6 @@
+var imie = prompt("Podaj imię");
+var nazwisko = prompt("Podaj nazwisko");
+
+document.write("Imię: "+ imie + "<br>Nazwisko: " + nazwisko);
+
+console.log("Twoje imię i nazwisko: " + imie +" "+ nazwisko);
---
+++ zadanie.js
@@ -0,0 +1,51 @@
+function dodawanie(){
+   var a = document.getElementsByName("a");
+    var b = document.getElementsByName("b");
+    a = a/1;
+    b = b/1;
+    alert("Wynik" + (a+b));  
+};
+
+
+function odejmowanie(){
+    
+    var a = document.getElementsByName("a");
+    var b = document.getElementsByName("b");
+    a = a/1;
+    b = b/1;
+    alert("Wynik" + (a-b));
+};
+
+
+function mnozenie(){
+
+    var a = document.getElementsByName("a");
+    var b = document.getElementsByName("b");
+    
+    a = a/1;
+    b =b/1;
+    alert("Wynik" + (a*b));    
+};
+
+
+function dzielenie(){
+    
+    var a = document.getElementsByName("a");
+    var b = document.getElementsByName("b");
+    a = a/1;
+    b = b/1;
+    alert("Wynik" + (a/b));
+};
+
+
+do{
+    var haslo = prompt("Podaj hasło");
+    
+}while(haslo != "qwerty");
+
+
+
+
+
+
+
