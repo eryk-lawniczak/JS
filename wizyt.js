c19a382c6bcdec89ee501a2d7bfd7ff36383c8f0
eryk-lawniczak <35136216+eryk-lawniczak@users.noreply.github.com>
2018-09-28 09:54:33 +0200

    Delete wizyt.js

--- wizyt.js
+++
@@ -1,6 +0,0 @@
-var imie = prompt("Podaj imię");
-var nazwisko = prompt("Podaj nazwisko");
-
-document.write("Imię: "+ imie + "<br>Nazwisko: " + nazwisko);
-
-console.log("Twoje imię i nazwisko: " + imie +" "+ nazwisko);
