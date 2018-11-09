c0aa975d4620aa3d27f3b035203c7f9f2409de09
eryk-lawniczak <35136216+eryk-lawniczak@users.noreply.github.com>
2018-11-09 09:37:58 +0100

    j

--- lekcja_5_kolko_krzyzyk/kolko.js
+++ lekcja_5_kolko_krzyzyk/kolko.js
@@ -12,6 +12,77 @@ function wczytajplansze() {
         td.addEventListener('click', kliknieciePola);
     }
 }
+function czyKoniecGry(pos_i, pos_j){
+  var kliknietePole = plansza[pos_i][pos_j].innerText.trim();
+
+  //poziomo
+  var czyZnalezionaLinia = true;
+  for(var j=0; j<3 && czyZnalezionaLinia ; j++)
+    if (plansza[pos_i][j].innerText.trim() != kliknietePole)
+      czyZnalezionaLinia = false;
+
+    if (czyZnalezionaLinia) {
+      for(var j=0; j<3 && czyZnalezionaLinia ; j++){
+          plansza[pos_i][j].classList.add("bg-yellow");
+          return true;
+      }
+    }
+
+
+  //pionowwo
+  var czyZnalezionaLinia = true;
+  for(var i=0; i<3 && czyZnalezionaLinia ; i++)
+    if (plansza[i][pos_j].innerText.trim() != kliknietePole)
+      czyZnalezionaLinia = false;
+
+    if (czyZnalezionaLinia) {
+      for(var i=0; i<3 && czyZnalezionaLinia ; i++){
+        plansza[i][pos_j].classList.add("bg-yellow");
+        return true;
+      }
+    }
+
+
+  //skosy
+  if (pos_i ==pos_j || 2-posi ==pos_j) {
+    var czyZnalezionaLinia = true;
+    for(var i=0; i<3 && czyZnalezionaLinia ; i++)
+      if (plansza[k][k].innerText.trim() != kliknietePole)
+        czyZnalezionaLinia = false;
+
+      if (czyZnalezionaLinia) {
+        for(var i=0; i<3 && czyZnalezionaLinia ; i++){
+          plansza[i][pos_j].classList.add("bg-yellow");
+          return true;
+        }
+      }
+      var czyZnalezionaLinia = true;
+      for(var i=0; i<3 && czyZnalezionaLinia ; i++)
+        if (plansza[i][i].innerText.trim() != kliknietePole)
+          czyZnalezionaLinia = false;
+
+        if (czyZnalezionaLinia) {
+          for(var i=0; i<3 && czyZnalezionaLinia ; i++){
+            plansza[2-i][i].classList.add("bg-yellow");
+            return true;
+          }
+        }
+    }
+    return false;
+  }
+
+  function czyJestRemis(){
+    var czyRemis = true;
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j<3; j++) {
+        if(!plansza[i].[j].classList.contains('fg-kolko') &&
+            !plansza[i].[j].classList.contains('fg-krzyzyk'));
+            czyRemis = false;
+      }
+    }
+
+  return czyRemis;
+  }
 
 function kliknieciePola(e) {
     var kliknietePole = e.target;
